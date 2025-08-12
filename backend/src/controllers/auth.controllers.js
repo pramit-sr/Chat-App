8bc5b6e221ef1499190e9fc7bb3885e72aa013d0
pramit_sr <mepramitsarkarr@gmail.com>
2025-08-12 11:47:03 +0530

    update Profile Done

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -90,11 +90,34 @@ export const logout = async(req, res) => {
       res.status(400).json({message: "Error in logging out"});
     }
 }
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
 
-export const updateProfile = async(req, res) => {
-  try{
-    const {profilePic} = req.body;
-  } catch(error){
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
 
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-}
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
